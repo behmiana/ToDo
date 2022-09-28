2dd6e0aafb21d6528f6def090bea6545215b3151
MIRLAN ALIMBEKOV <mirlanalimbekov@MIRLANs-MacBook-Air.local>
2022-09-28 14:57:10 +0300

    set media and static urls and roots

--- todo/urls.py
+++ todo/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from main.views import homepage, test, check
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
     path("test/", test, name="test"),
     path("check/", check)
-]
+]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
