8485a1e477f510c15a26be7b704daa527f0ff04c
MIRLAN ALIMBEKOV <mirlanalimbekov@MIRLANs-MacBook-Air.local>
2022-10-02 21:31:50 +0300

    modified:   homework/admin.py andmodified:   homework/models.py

--- homework/admin.py
+++ homework/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ToMeet
+ 
+admin.site.register(ToMeet)
 
 # Register your models here.
--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    date_of_meeting = models.DateTimeField(auto_now_add = False)
+    comment = models.TextField()
+    is_closed =models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
