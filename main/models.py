0f028ca47fa62af8d8c20e39caf8a79a80fe90ea
MIRLAN ALIMBEKOV <mirlanalimbekov@MIRLANs-MacBook-Air.local>
2022-10-02 20:35:17 +0300

    added todo model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+
+class todo(models.Model):
+    text= models.CharField(max_length=100)
+    date= models.DateTimeField(auto_now_add=True)
+    is_closed= models.BooleanField(default=False)
+    is_favorite= models.BooleanField(default=False)
+
