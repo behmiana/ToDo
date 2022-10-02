5d2e3f66900ff7761b8c5a11575db3d15954cd7e
MIRLAN ALIMBEKOV <mirlanalimbekov@MIRLANs-MacBook-Air.local>
2022-10-02 21:03:42 +0300

    changed containers and added icons

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import todo
 
 def homepage (request):
-    return render(request, "index.html")
+    todo_list = todo.objects.all()
+    return render(request, "index.html", {"todo_list": todo_list})
 
 def test(request):
     return render(request, "test.html")
